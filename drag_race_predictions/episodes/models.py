e9ffc6100ff6761cf83e8520bca96c47b1d6448d
Aster Ryan <aster.a.ryan@gmail.com>
2018-09-30 15:50:59 -0400

    Add a many-to-one relation between Episode and Season

--- drag_race_predictions/episodes/models.py
+++ drag_race_predictions/episodes/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from drag_race_predictions.seasons.models import Season
 
 class Episode(models.Model):
     title = models.CharField(max_length=255)
     original_air_date = models.DateField()
     episode_number = models.IntegerField()
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
